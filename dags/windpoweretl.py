7695753b5ad79ea3bd26c16b7531b3605d37d158
kaverapp-soul1 <timeflag.1985@gmail.com>
2025-07-09 22:23:21 +0530

    concern of separation

--- dags/windpoweretl.py
+++
@@ -1,7 +0,0 @@
-from airflow import DAG
-from airflow.providers.http.hooks.http import HttpHook
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.decorators import task
-from datetime import datetime, timedelta
-import requests
-import json
