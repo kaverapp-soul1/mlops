02ed43ed741c8f0747c282557b30d0cd34a980a7
kaverapp-soul1 <timeflagtbots@gmail.com>
2025-07-20 22:55:19 +0530

    refactor

--- api/main.py
+++ api/main.py
@@ -4,9 +4,7 @@ from fastapi.responses import JSONResponse
 app=FastAPI()
 
 
-@app.get("/")
-async def root():
-    return JSONResponse(content={"message": "Hello, World!"})
+
 
 @app.get("/health")
 async def health():
